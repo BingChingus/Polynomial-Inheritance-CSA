839a933e624cfb6d9d4ffc42d6680d7bb7aba3f5
BingChingus <124017700+BingChingus@users.noreply.github.com>
2023-03-12 13:38:49 -0500

    Add files via upload

--- PolynomialInheritanceRunner.java
+++ PolynomialInheritanceRunner.java
@@ -2,28 +2,36 @@ public class PolynomialInheritanceRunner
 {
     public static void main (String[] args)
     {
-        Polynomial p1 = new Polynomial(4, 2);
-        Polynomial p2 = new Polynomial(4, 2);
-        Polynomial p3 = new Polynomial(1, 2);
-        System.out.println(p1);
-        System.out.println(p1.equals(p2));
-        System.out.println(p1.equals(p3));
+        /*-----Test Cases-----*/
+        Polynomial polly = new Polynomial(5, 7);
+        System.out.println(polly);
 
-        Trinomial t1 = new Trinomial(5, 2, 1, 4);
-        Trinomial t2 = new Trinomial(5, 2, 1, 4);
-        Trinomial t3 = new Trinomial(4, -2, 1, 4);
-        System.out.println(t1);
-        System.out.println(t1.equals(t2));
-        System.out.println(t1.equals(t3));
+        Trinomial trinity = new Trinomial(4, 3, -5, 1);
+        System.out.println(trinity);
 
-        Quadratic q1 = new Quadratic(3, 1, 2);
-        Quadratic q2 = new Quadratic(3, 1, 2);
-        Quadratic q3 = new Quadratic(1, 1, -6);
-        System.out.println(q1);
-        System.out.println(q1.equals(q2));
-        System.out.println(q1.equals(q3));
-        System.out.println(q3.hasRealSolutions());
-        System.out.println(q1.hasRealSolutions());
-        System.out.println(q3.firstZero());
+        Quadratic quinn = new Quadratic(2, 5, -3);
+        System.out.println(quinn);
+
+        Polynomial pollySister = new Polynomial(5, 7);
+        System.out.println(polly.equals(pollySister));
+    
+        Trinomial trinityBrother = new Trinomial(4, 3, -5, 1);
+        System.out.println(trinity.equals(trinityBrother));
+
+        Quadratic quinnSister = new Quadratic(2, 5, -3);
+        System.out.println(quinn.equals(quinnSister)); 
+
+        System.out.println(quinn == quinnSister);
+
+        System.out.println(quinn.hasRealSolutions()); 
+
+        System.out.println(quinn.firstZero()); 
+
+        System.out.println(quinn.secondZero());
+
+        System.out.println(quinn.isPerfectSquare());
+
+        Quadratic quinton = new Quadratic(9, 12, 4);
+        System.out.println(quinton.isPerfectSquare());
     }    
 }
--- Quadratic.java
+++ Quadratic.java
@@ -5,7 +5,7 @@ public class Quadratic extends Trinomial
     public Quadratic(int a, int b, int c)
     {
         super(2, a, b, c);
-        discriminant = Math.pow((double) b,2.0) - 4 * (a * c);
+        discriminant = Math.pow(b,2.0) - 4 * (a * c);
     }
 
      /*-----Accessors-----*/
@@ -26,12 +26,26 @@ public class Quadratic extends Trinomial
 
     public double firstZero()
     {
-        return (-1 * super.getMidCoeff() + Math.sqrt(discriminant)) / (double) (2 * super.getLeadCoeff());
+        return (-1 * getMidCoeff() + Math.sqrt(discriminant)) / (double) (2 * getLeadCoeff());
+    }
+
+    public double secondZero()
+    {
+        return (-1 * getMidCoeff() - Math.sqrt(discriminant)) / (double) (2 * getLeadCoeff());
+    }
+
+    public boolean isPerfectSquare()
+    {
+        if (Math.pow(getMidCoeff(), 2) == 4 * getLeadCoeff() * getLastCoeff())
+        {
+            return true;
+        }
+        return false;
     }
 
     public String toString()
     {
-        return super.toString() + ", Formula: " + super.getLeadCoeff() + "x^2 + " + super.getMidCoeff() + "x + " + super.getLastCoeff();
+        return super.toString() + ", Formula: " + getLeadCoeff() + "x^2 + " + getMidCoeff() + "x + " + getLastCoeff();
     }
 
     public boolean equals(Quadratic q)
